f72dc3597cff950e1311ec53c03028393112fa26
Christopher Wagner <cwagne17@students.towson.edu>
2021-03-26 15:39:12 -0400

    reading and writing to file works

--- MergeSort.java
+++ MergeSort.java
@@ -1,34 +1,61 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
 import java.util.Scanner;
 import Algorithms.MergeSortAlgorithms;
 
 public class MergeSort{
-    private static int[] inputFromFile;
+    private static double[] inputFromFile;
+    private static final String DEFAULT_INPUT_FILE = "inputHW02.txt";
 
     public static void main(String[] args) {
         //Initialize Variables used in program
         Scanner scan = new Scanner(System.in);
         MergeSortAlgorithms mergeSortAlgorithms = new MergeSortAlgorithms();
+        String inputFileName = DEFAULT_INPUT_FILE;
+        int bool_randomFileNeeded;
 
         System.out.println("COSC-336 Homework 2 Merge Sort Program");
 
-        //Decides how to declare input array and declares input array
-        System.out.println("Where should the input come from?\n1 - inputHW02.txt\n2 - Randomized array of size n (will be asked for size n)");
-        int input = scan.nextInt();
-        switch(input){
-            case 1:
-                try {
-                    inputFromFile = getInputFromFile();
-                } catch (FileNotFoundException e) {
-                    System.out.println(e);
-                }
-                break;
-            case 2:
-                break;
-            default:
-                System.out.println(input+" Invalid Input");
+        //Retrieves whether new file needs to be created
+        do {
+            System.out.println("Does a file of random doubles need to be created?\n1 - Yes\n2 - No");
+            bool_randomFileNeeded = scan.nextInt();
+            if(bool_randomFileNeeded !=1 && bool_randomFileNeeded != 2){
+                System.out.println("ERROR: Invalid Input" + bool_randomFileNeeded);
+            }
+        } while(bool_randomFileNeeded !=1 && bool_randomFileNeeded != 2);
+       
+        //If new random double array is needed to be made these statements will execute
+        if(bool_randomFileNeeded == 1){
+            //Get size of array
+            System.out.println("How many doubles should populate the file?");
+            int arrayLength = scan.nextInt();
+            System.out.println("What is the name of the new file? (DO NOT INCLUDE .txt)");
+            inputFileName = scan.next() + ".txt";
+            try {
+                createRandomizedInputFile(arrayLength, inputFileName);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+
+        //Populates array of doubles depending on default file or file or randomized numbers
+        try {
+            inputFromFile = getInputFromFile(inputFileName);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        for(double num: inputFromFile){
+            System.out.println(num);
         }
 
         // //Decides the merge sort algorithm used
@@ -55,31 +82,50 @@ public class MergeSort{
     }
 
     /**
-    * Counts the amount of integers in a file and populates an int array
+     * Function will create/write to a new file the random double values
+     * @param N size of array
+     * @param name the file name that will be used
+     * @throws IOException
+     */
+    private static void createRandomizedInputFile(int N, String fileName) throws IOException {
+        Random randomGenerator = new Random();
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName));
+        
+        //writes random doubles to a text file max number is 3,000,000
+        for(int i = 0; i<N; i++){
+            double num = randomGenerator.nextDouble() * randomGenerator.nextInt(3000000);
+            printWriter.println(num);
+        }
+        printWriter.close();
+    }
+
+    /**
+    * Counts the amount of doubles in a file and populates an double array
     * runs at T(n) = 2n
     * Ex input: 1 2 323 53 1 43 
     */
-    private static int[] getInputFromFile() throws FileNotFoundException {
+    private static double[] getInputFromFile(String fileName) throws FileNotFoundException {
         Scanner counterScanner;
         Scanner fileScanner;
-        File file = new File("inputHW02.txt");
+        File file = new File(fileName);
         
-        //Counts amount of ints
+        //Counts amount of doubles
         counterScanner = new Scanner(file);
         int counter = 0;
-        while(counterScanner.hasNextInt()){
+        while(counterScanner.hasNextDouble()){
             counter++;
-            counterScanner.nextInt();
+            counterScanner.nextDouble();
         }
         counterScanner.close();
 
-        //Creates and populates int array
+        //Creates and populates double array
         fileScanner = new Scanner(file);
-        int[] intArray = new int[counter];
+        double[] doubleArray = new double[counter];
         for(int i =0; i<counter; i++){
-            intArray[i] = fileScanner.nextInt();
+            doubleArray[i] = fileScanner.nextDouble();
         }
         fileScanner.close();
-        return intArray;
+
+        return doubleArray;
     }
 }
